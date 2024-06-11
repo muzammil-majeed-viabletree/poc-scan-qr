fe61bcc18426586f847b8b00da4e8291f2847909
dev <>
2024-06-11 21:39:28 +0500

    small changes

--- src/App.js
+++ src/App.js
@@ -20,17 +20,23 @@ const App = (props) => {
 
   return (
     <>
-      <div style={{ height: "40vh", width: "70vh" }}>
+      <div style={{ height: "200px", width: "100%", border: "1px solid red" }}>
+        <p style={{}}>RESULT: {data}</p>
         <Scanner
           onResult={(text, result) => {
             setData(text);
             console.log(text, result);
+            // window?.postMessage(text);
+            if (window.ReactNativeWebView) {
+              // ensure window.ReactNativeWebView is there, otherwise, web app might crash if is not there
+              window.ReactNativeWebView.postMessage(text);
+            }
           }}
           onError={(error) => console.log(error?.message)}
         />
       </div>
 
-      <p style={{ marginTop: "45vh" }}>RESULT: {data}</p>
+      {/* {JSON.stringify(window)} */}
     </>
   );
 };
